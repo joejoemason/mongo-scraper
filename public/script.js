3d0c4c6620db539f10b5d371959b3bec9220f9c6
raphatapi <raphaeltapioca10@gmail.com>
2018-02-06 22:50:57 -0700

    article save and unsave

--- public/script.js
+++ public/script.js
@@ -1,9 +1,52 @@
 
+//Click to scrape
 $(document).on("click", ".scrape", function(){
-    $.get("/scrape", function (req, res) {
+    $(".articles").html("<img id='wait' src='./img/loading.gif'>");
+    $.get( "/scrape", function (req, res) {
         console.log(res);
     }).then(function(data) {
         window.location.href = "/";
     });
+});
+
+$(document).on("click", ".home", function(){
+    $.get( "/", function (req, res) {
+        console.log(res);
+    }).then(function(data) {
+        window.location.href = "/";
+    });
+});
+
+//Click to save article
+$(document).on("click", ".save", function(e){
+    $(this).parent().remove();
+    var articleId = $(this).attr("data-id");
+    $.ajax({
+        url: '/save/' + articleId,
+        type: "POST"
+    }).done(function(data) {
+        $(".article").filter("[data-id='" + articleId + "']").remove();
+    });
+});
+
+//Click to view saved article
+$(document).on("click", ".saved", function() {
+    $.get( "/saved", function (req, res) {
+        console.log(res);
+    }).then(function(data) {
+        window.location.href = "/saved";
+    });
   });
+
+//Click to delete article after being saved
+$(document).on("click", ".unsave", function(e){
+    $(this).parent().remove();
+    var articleId = $(this).attr("data-id");
+    $.ajax({
+        url: '/unsave/' + articleId,
+        type: "POST"
+    }).done(function(data) {
+        $(".article").filter("[data-id='" + articleId + "']").remove();
+    });
+})
 
--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(express.static("public"));
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+//Handlebars each_upto helper
 hbs.registerHelper('each_upto', function(ary, max, options) {
   if(!ary || ary.length == 0)
       return options.inverse(this);
@@ -37,8 +38,6 @@ hbs.registerHelper('each_upto', function(ary, max, options) {
   return result.join('');
 });
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
 var PORT = process.env.PORT || 3000;
 mongoose.Promise = Promise;
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
@@ -46,112 +45,94 @@ mongoose.connect(MONGODB_URI);
 
 // Routes
 
-// A GET route for scraping the echojs website
-app.get("/scrape", function(req, res) {
-  mongoose.connection.db.dropCollection('articles', function(err, result){
-		console.log('articles cleared...');
-  });
-
-  // First, we grab the body of the html with request
-  axios.get("https://www.nytimes.com/section/science").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article.story").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-      // Add the text and href of every link, and save them as properties of the result object
-      result.link = $(this).find("a").attr("href");
-      var title = $(this).find("h2").text();
-      result.title = title.trim();
-      result.summary = $(this).find("p.summary").text();
-      result.image = $(this).find("a").find("img").attr("src");
-      result.saved = false;
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article
-        .create(result)
-        .then(function(dbArticle) {
-          // If we were able to successfully scrape and save an Article, send a message to the client
-          res.end();
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          res.json(err);
-        });
-    });
-    res.redirect("/");
-  });
-});
-
 // Route for getting all saved Articles from the db
 app.get("/", function(req, res) {
-  // Grab every document that's saved in the Articles collection
   db.Article
     .find({saved: false})
     .then(function(dbArticle) {
       var hbsObject = {
         articles: dbArticle
       };
-      // If we were able to successfully find Articles, send them back to the client
       res.render("index", hbsObject);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-app.get("/saved", function(req, res) {
-  // Grab every document in the Articles collection
-  db.Article
-    .find({saved: true})
-    .then(function(dbArticle) {
-      res.json(dbArticles);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
+// A GET route for scraping the nytimes science website
+app.get("/scrape", function(req, res) {
+  var counter = 0;
+  axios.get("https://www.nytimes.com/section/science").then(function(response) {
+
+    var $ = cheerio.load(response.data);
+
+    $("article.story").each(function(i, element) {
+
+      var result = {};
+
+      result.link = $(this).find("a").attr("href");
+      result.title = $(this).find("h2").text().trim();
+      result.summary = $(this).find("p.summary").text();
+      result.image = $(this).find("a").find("img").attr("src");
+      result.saved = false;
+
+      if (result.title && result.link && result.summary) {
+        counter++;
+        db.Article
+        .create(result)
+        .then(function(dbArticle) {
+          res.send("You've scraped " + counter + " articles!");
+        })
+        .catch(function(err) {
+          res.json(err);
+        });
+      };
     });
+  });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/saved/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+// Route for grabbing a specific Article by id, update status to "saved"
+app.post("/save/:id", function(req, res) {
   db.Article
     .update({ _id: req.params.id }, { $set: {saved: true}})
-    // ..and populate all of the notes associated with it
     .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
+      res.json("dbArticle");
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note
-    .create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
+// Route for grabbing a specific Article by id, update status to "unsaved"
+app.post("/unsave/:id", function(req, res) {
+  db.Article
+    .update({ _id: req.params.id }, { $set: {saved: false}})
     .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
+      res.json("dbArticle");
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
+
+//Route to render Articles to handlebars and populate with saved articles
+app.get("/saved", function(req, res) {
+  db.Article
+  .find({ saved: true })
+  .then(function(dbArticles) {
+    var hbsObject = {
+      articles: dbArticles
+    };
+    res.render("saved", hbsObject);
+  })
+  .catch(function(err){
+    res.json(err);
+  });
+});
+
+
+
 
 // Start the server
 app.listen(PORT, function() {
